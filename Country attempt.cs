6b1227f568843ce16f8514c22509933a1eab9476
ZMantas <60478106+ZMantass@users.noreply.github.com>
2020-02-10 19:20:27 +0000

    Add files via upload

---
+++ Country attempt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Country
+{
+    // Conventions are as follows:
+    // camelCase for regular variables, uppercase initial letter only on methods and functions
+    // Add an _ for private variables
+    public string name { get; set; }
+    public int population { get; set; }
+    public bool eurozoneStatus { get; set; }
+
+    // Utilises a nullable bool for the vote, meaning we have three potential statuses of the bool.
+    private bool? _vote;
+    // This is perfect for the yes, no and abstain states of the vote.
+
+    public Country(string name, int population, bool eurozoneStatus)
+    {
+
+    }
+
+
+    public void ChangeVote(bool newVote)
+    {
+        _vote = newVote;
+    }
+
+    public bool? GetVote()
+    {
+        return _vote;
+    }
+}
