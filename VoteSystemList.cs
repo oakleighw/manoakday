8836a6ebc138dedf304cfc0f79f984007a691e3e
Oakleigh <oakleighweekes@hotmail.com>
2020-02-11 13:08:15 +0000

    tidied up Program class, changed country class to reflect merge

--- VoteSystemList.cs
+++ VoteSystemList.cs
@@ -3,57 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Voting;
+using CountryAttempt;
 
 namespace ConsoleApp6
 {
-    public class EUCountry
-    {
-        public EUCountry()
-        {
-            Name = null;
-        }
-        public EUCountry(string name, double population)
-        {
-            Name = Name;
-            Population = Population;
-          
-        }
-        public string Name { get; set; }
-        public double Population { get; set; }
-
+    public class Program{ 
 
 
         static void Main(string[] args)
         {
             List<Countries> countriesList = new List<Countries>();
 
-            countriesList.Add(new Countries() { Name = "Austria", Population = 1.98 });
-            countriesList.Add(new Countries() { Name = "Belgium", Population = 2.56 });
-            countriesList.Add(new Countries() { Name = "Bulgaria", Population = 1.56 });
-            countriesList.Add(new Countries() { Name = "Croatia", Population = 0.91 });
-            countriesList.Add(new Countries() { Name = "Cyprus", Population = 0.20 });
-            countriesList.Add(new Countries() { Name = "Czech Republic", Population = 2.35 });
-            countriesList.Add(new Countries() { Name = "Denmark", Population = 1.30 });
-            countriesList.Add(new Countries() { Name = "Estonia", Population = 0.30 });
-            countriesList.Add(new Countries() { Name = "France", Population = 14.98 });
-            countriesList.Add(new Countries() { Name = "Germany", Population = 18.54 });
-            countriesList.Add(new Countries() { Name = "Greece", Population = 2.40 });
-            countriesList.Add(new Countries() { Name = "Hungary", Population = 2.18 });
-            countriesList.Add(new Countries() { Name = "Ireland", Population = 1.10 });
-            countriesList.Add(new Countries() { Name = "Italy", Population = 13.65 });
-            countriesList.Add(new Countries() { Name = "Latvia", Population = 0.43 });
-            countriesList.Add(new Countries() { Name = "Lithuania", Population = 0.62 });
-            countriesList.Add(new Countries() { Name = "Luxembourg", Population = 0.14 });
-            countriesList.Add(new Countries() { Name = "Malta", Population = 0.11 });
-            countriesList.Add(new Countries() { Name = "Netherlands", Population = 3.89 });
-            countriesList.Add(new Countries() { Name = "Poland", Population = 8.49 });
-            countriesList.Add(new Countries() { Name = "Portugal", Population = 2.30 });
-            countriesList.Add(new Countries() { Name = "Romania", Population = 4.34 });
-            countriesList.Add(new Countries() { Name = "Slovakia", Population = 1.22 });
-            countriesList.Add(new Countries() { Name = "Slovenia", Population = 1.22 });
-            countriesList.Add(new Countries() { Name = "Spain", Population = 10.49 });
-            countriesList.Add(new Countries() { Name = "Sweden", Population = 2.29 });
+            countriesList.Add(new Country() { Name = "Austria", Population = 1.98 });
+            countriesList.Add(new Country() { Name = "Belgium", Population = 2.56 });
+            countriesList.Add(new Country() { Name = "Bulgaria", Population = 1.56 });
+            countriesList.Add(new Country() { Name = "Croatia", Population = 0.91 });
+            countriesList.Add(new Country { Name = "Cyprus", Population = 0.20 });
+            countriesList.Add(new Country { Name = "Czech Republic", Population = 2.35 });
+            countriesList.Add(new Country { Name = "Denmark", Population = 1.30 });
+            countriesList.Add(new Country { Name = "Estonia", Population = 0.30 });
+            countriesList.Add(new Country { Name = "France", Population = 14.98 });
+            countriesList.Add(new Country { Name = "Germany", Population = 18.54 });
+            countriesList.Add(new Country { Name = "Greece", Population = 2.40 });
+            countriesList.Add(new Country { Name = "Hungary", Population = 2.18 });
+            countriesList.Add(new Country { Name = "Ireland", Population = 1.10 });
+            countriesList.Add(new Country { Name = "Italy", Population = 13.65 });
+            countriesList.Add(new Country { Name = "Latvia", Population = 0.43 });
+            countriesList.Add(new Country { Name = "Lithuania", Population = 0.62 });
+            countriesList.Add(new Country { Name = "Luxembourg", Population = 0.14 });
+            countriesList.Add(new Country { Name = "Malta", Population = 0.11 });
+            countriesList.Add(new Country { Name = "Netherlands", Population = 3.89 });
+            countriesList.Add(new Country { Name = "Poland", Population = 8.49 });
+            countriesList.Add(new Country { Name = "Portugal", Population = 2.30 });
+            countriesList.Add(new Country { Name = "Romania", Population = 4.34 });
+            countriesList.Add(new Country { Name = "Slovakia", Population = 1.22 });
+            countriesList.Add(new Country { Name = "Slovenia", Population = 1.22 });
+            countriesList.Add(new Country { Name = "Spain", Population = 10.49 });
+            countriesList.Add(new Country { Name = "Sweden", Population = 2.29 });
 
             foreach (var country in countriesList)
 
@@ -77,9 +63,10 @@ namespace ConsoleApp6
                 }
                 else
                 {
-                    
+
                 }
                 // set up voting rule section
+            }
             }
         }
     }
